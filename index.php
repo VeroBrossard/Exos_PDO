35e83211e9a3d07f7b112b049cbc2d2b56d97cc5
Véro Brossard <brossard.vero.pro@laposte.net>
2021-02-26 15:35:32 +0100

    initialisation

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/style.css">
+  <title>Colyseum</title>
+</head>
+
+<body>
+  <div id="header"><span id="title1"> Colyseum </span>
+  </div>
+
+  <section>
+  <?php 
+
+
+  // affichage des attributs de $compte1 avec leurs valeurs
+  //$Compte1->test();
+  ?>
+    <h1></h1>
+    <div>
+
+      <BR>
+    </div>
+
+    <div>
+
+    </div>
+    <br><br>
+    Bonne journée à vous ...
+    <br><br>
+  </section>
+</body>
+
+</html>
